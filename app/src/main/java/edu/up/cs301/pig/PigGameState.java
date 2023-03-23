cb95bf26ea5076a880ea39c13a01457ad53429d0
Alexis Nguyen <alexisnguyen18@icloud.com>
2023-03-23 16:13:42 -0700

    Initial commit

---
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -0,0 +1,12 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.infoMsg.GameState;
+
+public class PigGameState extends GameState {
+
+    private int playerid = 0;
+    private int player0 = 0;
+    private int player1 = 0;
+    private int currentTotal = 0;
+    private int currentVal = 0;
+}
